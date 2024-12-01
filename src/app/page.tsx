4b585f4fa8c2ca29c8519c75f6b0576aee6baa22
Eddie <eddie.zeng95@gmail.com>
2024-12-01 19:33:57 +1100

    add oauth account linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+'use server';
+import { LinkAccountButton } from "@/components/link-account-button";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  return <Button>Hello World</Button>;
+  return <LinkAccountButton />;
 }
